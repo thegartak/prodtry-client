ce64faf7ec8b5f48cf1ca41ad9627d405b66880f
thegartak <agevorg380@student.glendale.edu>
2024-10-16 18:15:13 +0400

    look at this one

--- src/components/SomeModule.tsx
+++ src/components/SomeModule.tsx
@@ -35,7 +35,7 @@ const SomeModule = () => {
  //   someFnc();
 
     return (
-        <><h1>H1 Tag has changed with ag83</h1></>
+        <><h1>H1 Tag has changed with ag83 and look at new one</h1></>
     )
    
 }
