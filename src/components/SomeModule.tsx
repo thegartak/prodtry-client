ce9f4754831d5357896d6f663c2b6590fd14929c
thegevorgyan <g.artak@yahoo.com>
2024-10-17 18:16:00 +0400

    added h2 tag

--- src/components/SomeModule.tsx
+++ src/components/SomeModule.tsx
@@ -35,7 +35,10 @@ const SomeModule = () => {
  //   someFnc();
 
     return (
-        <><h1>H1 Tag has changed the second time</h1></>
+        <>
+          <h1>H1 Tag has changed the second time</h1>
+          <h2>let's add h2 tag</h2>
+        </>
     )
    
 }
